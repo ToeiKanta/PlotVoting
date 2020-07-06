1e3a86add7bd6b799f257e0a5c148ad26a499d33
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-07-07 05:38:57 +0700

    setup memory db

--- src/main/java/net/toeikanta/multiplex/DatabaseHandler.java
+++ src/main/java/net/toeikanta/multiplex/DatabaseHandler.java
@@ -1,18 +1,43 @@
 package net.toeikanta.multiplex;
 
-import code.nextgen.sqlibrary.database.DatabaseConnection;
-import code.nextgen.sqlibrary.database.SQLiteConnection;
-
-import java.io.File;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseHandler {
-    //Creating a SQLiteConnection
-    DatabaseConnection connection;
+    private final String dbFileName = "/database.db";
+    private PlotVoting plotVoting;
 
-    public void connectDB() throws SQLException, ClassNotFoundException {
-        this.connection = new SQLiteConnection(new File("my.db"));
-        //Opening a DatabaseConnection
-        this.connection.openConnection();
+    public DatabaseHandler(PlotVoting plotVoting){
+        this.plotVoting = plotVoting;
+        this.connect();
+    }
+
+    public void connect() {
+        Connection conn = null;
+        try {
+            // create databaseFile if it's not exist.
+            File databaseFile = new File(plotVoting.getDataFolder() +  dbFileName);
+            if(!databaseFile.exists()){
+                databaseFile.mkdirs();
+            }
+            // create a connection to the database
+//            String url = "jdbc:sqlite:" + plotVoting.getDataFolder() + dbFileName;
+            String url = "jdbc:sqlite:memory:";
+//            System.out.println(url);
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
--- src/main/java/net/toeikanta/multiplex/PlotVoting.java
+++ src/main/java/net/toeikanta/multiplex/PlotVoting.java
@@ -6,8 +6,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Multiplex extends JavaPlugin {
-
+public final class PlotVoting extends JavaPlugin {
+    private DatabaseHandler db;
 
     @Override
     public void onEnable() {
@@ -20,13 +20,12 @@ public final class Multiplex extends JavaPlugin {
         getLogger().info("onEnable has been invoked!");
         getLogger().info("onEnable has been invoked!");
         getLogger().info("onEnable has been invoked!");
+        db = new DatabaseHandler(this);
         this.getCommand("pvote").setExecutor(this);
-        // TODO : Do something if your plugin needs it (registering commands / listeners)
     }
 
     @Override
     public void onDisable() {
-        // TODO : Do something if your plugin needs it (saving custom configs, clearing cache, closing connections...)
     }
 
     String pvote = "pvote";
